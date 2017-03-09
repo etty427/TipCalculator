af81cd1082048fabc256ff16aec34000365fe651
etty427 <etty427@aim.com>
2017-03-09 00:06:44 -0500

    First part of calculator completed.

---
+++ tippy/SettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsViewController.swift
+//  tippy
+//
+//  Created by Etnuh on 3/8/17.
+//  Copyright © 2017 Rainman Technologies. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var defaultSwitch: UISwitch!
+    @IBOutlet weak var defaultControl: UISegmentedControl!
+    @IBOutlet weak var darkThemeSwitch: UISwitch!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
